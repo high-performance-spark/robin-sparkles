4fac19b05d2dbc2bda7802540b19c1b4d41fd779
Holden Karau <holden@pigscanfly.ca>
2018-03-10 16:38:18 -0800

    Use spark-measure

--- src/main/scala/com/highperformancespark/robinsparkles/CountingApp.scala
+++ src/main/scala/com/highperformancespark/robinsparkles/CountingApp.scala
@@ -1,4 +1,4 @@
-package com.highperformancespark.robinSparkles
+package com.highperformancespark.robinsparkles
 
 import org.apache.spark.{SparkConf, SparkContext}
 
--- src/main/scala/com/highperformancespark/robinsparkles/WordCount.scala
+++ src/main/scala/com/highperformancespark/robinsparkles/WordCount.scala
@@ -1,4 +1,4 @@
-package com.highperformancespark.robinSparkles
+package com.highperformancespark.robinsparkles
 
 /**
  * Everyone's favourite wordcount example.
